0a92b662faa23f7037d8dd9e74044f1f45301d29
isaac k. kimura <isaackimura74@gmail.com>
2023-04-03 12:03:33 +0300

    Project set up

---
+++ test/main_tests.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+// Test suite
+describe('Mocha', () => {
+  // Test spec(unit tests)
+  it('should run our tests using npm', () => {
+    expect(true).to.be.ok;
+  });
+});
